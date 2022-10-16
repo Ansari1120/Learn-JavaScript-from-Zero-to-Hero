70a59ac6133794da9fcca78bf967d88536fb6f2c
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2022-10-16 07:31:15 +0500

    new files

---
+++ template literals.js
@@ -0,0 +1,29 @@
+var name = `My name is : 
+Ahmed`;
+
+console.log(name);
+
+var name8 = "ab"
+console.log(name8);
+
+function abc(){
+    var name9 = "sir zia";
+    console.log(name9);
+}
+abc();
+
+console.log(name9);  //undefined error name9 is blocked scope 
+
+function efg(){
+     name10 = "sir maaz";
+    console.log(name10);
+}
+efg();
+
+console.log(name10);
+
+//= assignment operator
+
+//== compare values
+
+//=== compare values and datatypesl
